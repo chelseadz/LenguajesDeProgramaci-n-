6d3f25a1bbbd9fc9d4f3f6ea90ea24a01cbc9154
chelseadz <83433523+chelseadz@users.noreply.github.com>
2022-09-07 19:44:53 -0700

    delete

--- tarea-02/gcdgenerative.rkt
+++
@@ -1,6 +0,0 @@
-(define (gcd-generative n m)
-  (define (find-largest-divisor max min)
-    (if (= min 0)
-        max
-        (find-largest-divisor min (remainder max min))))
-  (find-largest-divisor (max n m) (min n m)))
